a01228e1291285a5969f7a9066660611a38ec13f
ZacharyJC02 <64038501+ZacharyJC02@users.noreply.github.com>
2022-03-31 17:30:06 -0700

    Add files via upload

---
+++ PUSHSENS.ino
@@ -0,0 +1,27 @@
+//define LED port 
+int Led = 13; 
+//define shock port 
+int Shock = 2; 
+//define digital variable val 
+int val; 
+void setup() 
+{ 
+//define LED as a output port 
+pinMode(Led, OUTPUT); 
+//define shock sensor as a output port 
+pinMode(Shock,INPUT); 
+} 
+void loop() 
+{ 
+//read the value of the digital interface 3 assigned to val 
+val = digitalRead(Shock); 
+//when the shock sensor have signal, LED blink 
+if (val == HIGH) 
+{ 
+digitalWrite(Led, LOW); 
+} 
+else 
+{ 
+digitalWrite(Led, HIGH); 
+} 
+} 
